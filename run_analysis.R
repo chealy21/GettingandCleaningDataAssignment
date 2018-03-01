4da61f51eda12188095043860dcbcf7a266a3425
chealy21 <35117439+chealy21@users.noreply.github.com>
2018-02-28 22:16:13 -0500

    Add files via upload
    Files uploaded 2018-02-28

---
+++ run_analysis.R
@@ -0,0 +1,64 @@
+run_analysis<-function(){
+        require(dplyr)
+        require(reshape2)
+        
+        #download zip file
+        download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile = "UCI HAR Dataset.zip")
+        
+        #unzip file
+        unzip("UCI HAR Dataset.zip")
+        
+        #read files
+        activitylabels<-read.table("./UCI HAR Dataset/activity_labels.txt")
+        featurelabels<-read.table("./UCI HAR Dataset/features.txt")
+        testsubjects<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+        testdata<-read.table("./UCI HAR Dataset/test/X_test.txt")
+        testactivity<-read.table("./UCI HAR Dataset/test/y_test.txt")
+        trainsubjects<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+        traindata<-read.table("./UCI HAR Dataset/train/X_train.txt")
+        trainactivity<-read.table("./UCI HAR Dataset/train/y_train.txt")
+        
+        #apply features as column names
+        names(testdata)<-featurelabels[,2]
+        names(traindata)<-featurelabels[,2]
+        
+        #label subject and activity
+        names(testsubjects)<-"subject"
+        names(testactivity)<-"activity"
+        
+        names(trainsubjects)<-"subject"
+        names(trainactivity)<-"activity"
+        
+        #add activity description
+        names(activitylabels)<-c("activity","descriptionofactivity")
+        
+        #update activity to remove underscores
+        activitylabels<-mutate(activitylabels,description=gsub("_"," ",description))
+        
+        #join activities
+        testactivity<-left_join(testactivity,activitylabels,by="activity")
+        trainactivity<-left_join(trainactivity,activitylabels,by="activity")
+        
+        #subset mean and std
+        summarytestdata<-testdata[,grep("mean|std",colnames(testdata))]
+        summarytraindata<-traindata[,grep("mean|std",colnames(traindata))]
+        
+        #create test dataframe
+        testdf<-cbind(testsubjects,testactivity,summarytestdata)
+        
+        #create train dataframe
+        traindf<-cbind(trainsubjects,trainactivity,summarytraindata)
+        
+        #rbind test and train
+        summarydata<-rbind(testdf,traindf)
+        
+        #melt
+        summarymelt<-melt(summarydata,id=c("subject","activity","descriptionofactivity"))
+        
+        #dcast to get mean by activity and subject
+        summarytable<-dcast(summarymelt,subject+descriptionofactivity~variable,mean)
+        
+        #write table
+        write.table(summarytable,file="summarytable.txt", sep=" ",col.names=TRUE)
+        
+}
